b53b548a1663ff7560acb55dc201113f47cfda08
vladimirovicp <vladimirovicp70@gmail.com>
2025-04-04 14:46:21 +0400

    feat: config experiment

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,8 +1,12 @@
 const ourStorySidebar = [{ text: 'Our Story', link: '/about/our-story'}]
 
 module.exports = {
+    lang: 'ru-RU',
     title: 'Alt Linux container guide',
+    description: 'Описание сайта.',
+    logo: '/logo.png',
     themeConfig: {
+        // параметры темы
         nav: [
             {text: 'Home', link: '/'},
             { text: 'About', link: '/about/'},
